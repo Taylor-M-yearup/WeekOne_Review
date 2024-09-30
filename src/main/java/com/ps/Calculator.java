e665820c98783d75f85d8916c451357a17ef36e8
Taylor <taylorsmayberry18@gmail.com>
2024-09-30 11:12:56 -0500

    first commit

---
+++ src/main/java/com/ps/Calculator.java
@@ -0,0 +1,27 @@
+package com.ps;
+
+class Calculator {
+    public static void main(String[] args){
+ int sum = addition(200,  500);
+ System.out.println(sum);
+    }
+
+    //Static add method
+    public static int addition(int numb1, int num2){
+        return numb1 + num2;
+    }
+    //Static subtract method
+    //Static multiplication method
+    // Static division method
+
+    //Receive two inputs (ints)
+    //Return a single number (int, double)
+
+
+    //Extras
+    //Static module method
+    //Three inputs
+
+    //Calculate area
+    //Calculate perimeter
+}
